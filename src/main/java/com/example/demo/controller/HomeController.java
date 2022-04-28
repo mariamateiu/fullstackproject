d7303c7333617376ab996f3d592ca22d74ba1c3e
naomi <naom0649@stud.kea.dk>
2022-04-28 13:12:24 +0200

    localhost virker til alle sider

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -3,18 +3,58 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import utility.ConnectionManager;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String hello(){
+    public String vedikke(){
+        ConnectionManager.connectionToDB();
+
+        return "vedikke";
+    }
+    @GetMapping("/TilHam")
+    public String TilHam(){
+
         return "TilHam";
     }
+    @GetMapping("/TilHende")
+    public String TilHende(){
+        return "TilHende";
+    }
+    @GetMapping("/TilParret")
+    public String TilParret(){
+
+        return "TilParret";
+    }
+    @GetMapping("/TilBarnet")
+    public String TilBarnet(){
+
+        return "TilBarnet";
+    }
+
+    @GetMapping("/Oprettelse")
+    public String Oprettelse(){
+
+        return "Oprettelse";
+    }
+    @GetMapping("/OpretØnskeliste")
+    public String OpretØnskerliste(){
+
+        return "OpretØnskerliste";
+    }
+
+    @GetMapping("/Forside")
+    public String Forside(){
+
+        return "Forside";
+    }
+
 
     @GetMapping("/login")
     public String loginSide(){
-        return "Login";
+        return "EfterLogin";
     }
 
 }
---
+++ src/main/java/utility/ConnectionManager.java
@@ -0,0 +1,45 @@
+package utility;
+
+import java.sql.*;
+
+public class ConnectionManager {
+    private static Connection connection;
+    private Statement myStmt;
+    private ResultSet rs;
+
+
+    public static Connection connectionToDB () {
+
+
+        String url = "jdbc:mysql://localhost:3306/ønsker";
+        String user = "root";
+        String password = "N123naomi";
+
+        try {
+                connection = DriverManager.getConnection(url, user, password);
+                System.out.println("we have a connection");
+        } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("somethings wrong with the connection");
+        }
+        return connection;
+
+    }
+
+    public void insertInto () {
+        String sql = "select * from ønsker.opret_ønsker";
+
+        try {
+            myStmt = connection.createStatement();
+            myStmt.executeUpdate("insert into opret_ønsker" + "(first_name,last_name)" + "values ('maria','lone')");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println("all good");
+        //ResultSet rs = myStmt.executeQuery(sql);
+    }
+
+
+}
